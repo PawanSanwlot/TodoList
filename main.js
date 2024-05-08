fdbdd849b089efcc032646995f0a799754c253b4
Ryker <82869753+PawanSanwlot@users.noreply.github.com>
2024-05-08 12:48:36 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+document.getElementById('app').innerHTML = `
+  <div class="container">
+    <h1>My Todo list</h1>
+    <h2>Tasks</h2>
+    <ul>
+      <li>Wake Up Early</li>
+      <li>Gey fresh</li>
+      <li>Exersice</li>
+      <li>Drink water</li>
+    </ul>
+
+    <h2>Completed Tasks</h2>
+  </div>
+`
+
+
