a31b73a0aaa997d0a3e80f06b07003379668c044
Ryker <82869753+PawanSanwlot@users.noreply.github.com>
2024-05-13 11:22:58 +0530

    added todos array

--- main.js
+++ main.js
@@ -1,48 +1,102 @@
 import './style.css'
+import { v4 as uuidv4 } from 'uuid';
+
+const todos = [
+  {
+    id: uuidv4(),
+    task: "Run",
+    createdAt: Date.now(),
+    isFinished: false
+  }
+]
+
+const todoLiElements = todos.map((todo) => {
+  return `<div class="task-container">
+      <li class="list-group-item task">${todo.task}</li>
+      <button class="btn btn-outline-danger btn-sm delete-btn" id=${`${uuidv4()}`}>Delete</button>
+    </div>`
+})
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
     <h1 class="heading">My Todo list!</h1>
-
     <input type="text" placeholder="Add Task" class="form-control add-task-input">
     <button id="add-btn"  class="btn btn-success">Add</button>
-    <ul class="list-group">
-      <div class="task-container">
-        <li class="list-group-item">Wake Up Early</li>
-        <button class="btn btn-outline-danger btn-sm">Delete</button>
-      </div>
-      <div class="task-container">
-        <li class="list-group-item">Wake Up Early</li>
-        <button class="btn btn-outline-danger btn-sm">Delete</button>
-      </div>
-      <div class="task-container">
-        <li class="list-group-item">Wake Up Early</li>
-        <button class="btn btn-outline-danger btn-sm">Delete</button>
-      </div>
-      <div class="task-container">
-        <li class="list-group-item">Wake Up Early</li>
-        <button class="btn btn-outline-danger btn-sm">Delete</button>
-      </div>
-    </ul>
-
+    <ul class="list-group">${todoLiElements.join("")}</ul>
   </div>
 `
-
+const ul = document.querySelector(".list-group")
 const addTaskBtn = document.getElementById('add-btn')
 const taskInput = document.querySelector(".add-task-input")
 
 function handleClick() {
-  console.log("button was clicked!")
+  if(taskInput.value !== "") {
+    // Making a list Item
+
+    // 1. Creating Elements
+    const div = document.createElement("div")
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+
+    // 2. Placing Elements accordingly
+    ul.appendChild(div)
+    div.appendChild(li)
+    div.appendChild(btn)
+
+    // 3. Adding text accordingly
+    btn.textContent = "Delete"
+    li.textContent = taskInput.value
+
+    // 4. add classes to the elements
+    div.classList.add("task-container")
+    li.classList.add("list-group-item", "task")
+    btn.classList.add("btn", "btn-outline-danger", "btn-sm", "delete-btn")
+    
+    // pushing task data to the todos array 
+    todos.push({
+      task: taskInput.value,
+      createdAt: Date.now(),
+      isFinished: false
+    })
+
+    // clear the input after makin the list item
+    taskInput.value = ""
+  }
 }
 
 function handleInput(event){
-  console.log("user is typing..")
-  console.log(event.target.value)
+  // console.log("user is typing..")
+  // console.log(event.target.value)
 }
 
 addTaskBtn.addEventListener("click", handleClick)
 taskInput.addEventListener("input", handleInput)
 
+document.addEventListener("click", (event) => { 
+  if (event.target.classList.contains("delete-btn")) {
+    event.target.parentNode.remove()
+  }
+  if (event.target.classList.contains("task")) {
+    event.target.classList.toggle("done")
+  }
+})
+
+finc
+
+// edit button made by only js
+ 
+// // 1. create a btn from js
+// const btn = document.createElement("button")
+// // 2. give text content to btn
+// btn.textContent = "Edit"
+// // 3. place btn in body element
+// document.body.appendChild(btn)
+// // 4. give some css style by js
+// btn.style.color = "white"
+// btn.style.backgroundColor = "black"
+// btn.style.margin = "10px"
+// btn.style.padding = "10px"
+// btn.style.borderRadius = "10px"
 
 
 
@@ -99,6 +153,9 @@ taskInput.addEventListener("input", handleInput)
 //   bodyCount: 19,
 //   name: "james goldberg",
 //   isCriminal: true,
+//   getCurrentNews: function () {
+//     console.log("current news")
+//   },
 //   crimes: ["murder", "theft", "kidnapping"],
 //   connectionsWithOtherCriminals: {
 //     criminals: ["jeffery damour", "freddy cougar", "katherine thomas"],
@@ -106,7 +163,11 @@ taskInput.addEventListener("input", handleInput)
 //   }
 // }
 
-// console.log( james.connectionsWithOtherCriminals.criminals[2])
+// console.log(james.getCurpushrentNews())
+
+// const array = [1,2,3]
+
+// array.push(9)
 
 
 
